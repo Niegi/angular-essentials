34808d041a17910781eab3d813c2d0294cc15b68
Niegowski Tomasz-PNM386 <Tomasz.Niegowski@motorolasolutions.com>
2019-10-30 15:47:57 +0100

    after lesson 51

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  rootItems = ['Apples', 'Bananas', 'Cherries'];
-
-  onItemWasAdded(newItem) {
-    this.rootItems.push(newItem);
-    console.log(this.rootItems);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CartComponent } from './cart/cart.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent
+    TabsComponent,
+    ListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
