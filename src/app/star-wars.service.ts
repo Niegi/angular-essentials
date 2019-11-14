e82b9d708d139f712e0a6a9cef0565bb19e97c2f
Niegowski Tomasz-PNM386 <Tomasz.Niegowski@motorolasolutions.com>
2019-11-14 16:38:09 +0100

    Reactivity module finished

--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,5 +1,6 @@
 import { LogService } from "./log.service";
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs/Subject";
 
 @Injectable()
 export class StarWarsService {
@@ -8,6 +9,7 @@ export class StarWarsService {
     { name: 'Darth Vader', side: '' }
   ];
   private logService: LogService;
+  charactersChanged = new Subject<void>();
 
   constructor(logService: LogService) {
     this.logService = logService;
@@ -28,6 +30,7 @@ export class StarWarsService {
       return char.name === charInfo.name;
     })
     this.characters[pos].side = charInfo.side;
+    this.charactersChanged.next();
     this.logService.writeLog('Changed side for: ' + charInfo.name + ', new side: ' + charInfo.side);
   }
 
